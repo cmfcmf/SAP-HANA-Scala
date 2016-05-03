9e043088043f35db34a9e10c1e9e66915e3e7465
Christian Flach <cmfcmf.flach@gmail.com>
2016-05-03 11:54:52 +0200

    Automatically open and close the database connection in our controller.

--- src/main/scala/de/hpi/callcenterdashboard/CallcenterServlet.scala
+++ src/main/scala/de/hpi/callcenterdashboard/CallcenterServlet.scala
@@ -1,19 +1,32 @@
 package de.hpi.callcenterdashboard
 
+import javax.servlet.ServletConfig
+
 import org.scalatra._
 import org.scalatra.scalate.ScalateSupport
 
 class CallcenterServlet extends CallcenterDashboardStack with ScalateSupport {
-
-  val connection = new DatabaseConnection()
-
   get("/") {
     contentType="text/html"
 
     layoutTemplate("/index")
   }
   post("/find-customer") {
-
+    databaseConnection.open()
+    databaseConnection.printCustomers()
   }
 
+  val databaseConnection = new DatabaseConnection()
+
+  override def init(config: ServletConfig): Unit = {
+    super.init(config)
+
+    databaseConnection.open()
+  }
+
+  override def destroy(): Unit = {
+    super.destroy()
+
+    databaseConnection.close()
+  }
 }
--- src/main/scala/de/hpi/callcenterdashboard/DatabaseConnection.scala
+++ src/main/scala/de/hpi/callcenterdashboard/DatabaseConnection.scala
@@ -6,25 +6,36 @@ class DatabaseConnection {
   val credentials = new Credentials()
   val driver = "com.sap.db.jdbc.Driver"
   val url = "jdbc:sap://" + credentials.hostname + ":" + credentials.port
-  var con: Option[Connection] = None
-  try {
-    // make the connection
-    Class.forName(driver)
-    con = Some(DriverManager.getConnection(url, credentials.username, credentials.password))
-    con.map(connection => {
+  var connection = None : Option[Connection]
+
+  def open(): Unit = {
+    try {
+      // make the connection
+      Class.forName(driver)
+      connection = Some(DriverManager.getConnection(url, credentials.username, credentials.password))
+
+    } catch {
+      // connection.get.close()
+      case e: Throwable => e.printStackTrace()
+    }
+  }
+
+  def close(): Unit = {
+    connection.get.close()
+  }
+
+  def printCustomers(): Unit = {
+    try {
       // create the statement, and run the select query
-      val statement = connection.createStatement()
-      //val resultSet = statement.executeQuery("SELECT RBUKRS, BUDssfAT, KSL, RKCUR FROM ACDOCA LIMIT 100")
+      val statement = connection.get.createStatement()
       val resultSet = statement.executeQuery("SELECT * FROM SAPQ92.KNA1_HPI LIMIT 100")
       while (resultSet.next()) {
         val knd_name = resultSet.getString("NAME")
         println(knd_name)
       }
-    })
-  } catch {
-    case e: Throwable => e.printStackTrace
-  } finally {
-    con.map(_.close())
+    } catch {
+      case e: Throwable => e.printStackTrace()
+    }
   }
 }
 
