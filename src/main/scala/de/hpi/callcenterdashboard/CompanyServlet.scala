05e38381002bf1d7e790b4d3a61928cbe9438c20
julianweise <privat@julianweise.de>
2016-05-25 13:48:58 +0200

    Fixed View of placeholder start- & enddate

---
+++ src/main/scala/de/hpi/callcenterdashboard/CompanyServlet.scala
@@ -0,0 +1,10 @@
+package de.hpi.callcenterdashboard
+
+import org.scalatra.{ScalatraParams, SessionSupport}
+import org.scalatra.scalate.ScalateSupport
+
+class CustomerServlet extends DataStoreAwareServlet with ScalateSupport {
+
+  
+
+}
